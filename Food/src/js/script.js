07f4b68b53a18d69a68e7a50d54d8d5fb7157763
Andrei <65337938+tarantinolj@users.noreply.github.com>
2022-11-12 18:58:31 +0300

    + timer Clock ...
    add timer clock / setInterval / clearInterval

--- Food/src/js/script.js
+++ Food/src/js/script.js
@@ -33,5 +33,41 @@ window.addEventListener('DOMContentLoaded', ()=>{
                 });
            };
     });
-          
+     
+    const deadline = '2023-03-15';
+
+    function getTimeRemaining (endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t/(1000*60*60*24)),
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((t / (1000 * 60)) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        }
+    }
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = document.querySelector('#days'),
+              hours = document.querySelector('#hours'),
+              minutes = document.querySelector('#minutes'),
+              seconds = document.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+            if (t.total <=0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadline); 
 });
